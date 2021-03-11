2791c4eae7cbdf113c1581a5d42a842c20ed57ca
neolardo <filep.leonardo@gmail.com>
2021-03-11 17:30:17 +0100

    RunStatechart modified

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,35 +1,57 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
-
 
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		while(true)
+		{
+			cycle(s);
+		}
 	}
 
 	public static void print(IExampleStatemachine s) {
 		System.out.println("W = " + s.getSCInterface().getWhiteTime());
 		System.out.println("B = " + s.getSCInterface().getBlackTime());
+	}
+	
+	public static void cycle(ExampleStatemachine s)
+	{
+		Scanner sc= new Scanner(System.in);
+		String str= sc.nextLine();
+		print(s);
+		if(str.equals("start"))
+		{
+			s.raiseStart();
+			s.runCycle();	
+		}
+		else if(str.equals("white"))
+		{
+			s.raiseWhite();
+			s.runCycle();	
+		}
+		else if(str.equals("black"))
+		{
+			s.raiseBlack();
+			s.runCycle();	
+		}
+		else if(str.equals("exit"))
+		{
+			System.exit(0);
+		}
 	}
 }
